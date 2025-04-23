28faf7f65820151a529313f4c16e768fb0afdb9f
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2025-04-23 11:27:26 +0300

    feat & fix: Added missing babel.config.js file. Also rewrote metro.config.js and cleared caches.

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,8 +1,13 @@
 //This is compulsory react native SVG transformer and render for the project.
-
 //Source: https://github.com/kristerkari/react-native-svg-transformer?tab=readme-ov-file
 
+//For reanimated plugin, please read documentations:
+//https://docs.expo.dev/versions/latest/config/babel/
+//https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/getting-started/
+
 const { getDefaultConfig } = require("expo/metro-config");
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
 
 module.exports = (() => {
     const config = getDefaultConfig(__dirname);
@@ -19,5 +24,5 @@ module.exports = (() => {
         sourceExts: [...resolver.sourceExts, "svg"]
     };
 
-    return config;
+    return wrapWithReanimatedMetroConfig(config);
 })();
